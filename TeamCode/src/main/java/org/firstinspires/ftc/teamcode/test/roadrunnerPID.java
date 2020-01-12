2a64df531ec552003c6017becfa239759c502ca3
FTC2875 <2875mitibots@gmail.com>
2020-01-12 17:01:21 -0500

    1/12

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/roadrunnerPID.java
@@ -0,0 +1,21 @@
+//package org.firstinspires.ftc.teamcode.test;
+//
+//import com.acmerobotics.roadrunner.control.PIDCoefficients;
+//import com.acmerobotics.roadrunner.control.PIDFController;
+//
+//public class roadrunnerPID {
+//
+//    // specify coefficients/gains
+//    PIDCoefficients coeffs = new PIDCoefficients(kP, kI, kD);
+//    // create the controller
+//    PIDFController controller = new PIDFController(coeffs);
+//
+//// specify the setpoint
+//
+//controller.setTargetPosition(setpoint);
+//
+//    // in each iteration of the control loop
+//// measure the position or output variable
+//// apply the correction to the input variable
+//    double correction = controller.update(measuredPosition);
+//}
